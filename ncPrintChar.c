a9719fbe6796ff4f1a826842e415e6921bceb8cd
skr4ll <skrall_pa@protonmail.com>
2023-11-21 20:08:46 +0100

    Added Colour changer

--- ncPrintChar.c
+++ ncPrintChar.c
@@ -1,26 +1,63 @@
 #include <stdio.h>
 #include <ncurses.h>
+int selectColour(char c);
 
 int main(){
 	char test = 'a';
+	
 	initscr();	//Cursemodus starten
 	raw();		//Raw ermöglicht direkten Zugriff auf Usereingabe, ohne auf new line zu warten
 	noecho();	//noecho unterdrückt die direkte Wiedergabe des Inputs
+	
 	start_color();
-	init_pair(1, COLOR_RED, COLOR_WHITE);
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);
+	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(4, COLOR_WHITE, COLOR_BLACK);
+	init_pair(5, COLOR_CYAN, COLOR_BLACK);
+
 	while(true){
 		if(test == 'q'){	//Ist die Eingabe q, beenden wir das Programm
 			endwin();
 			return 0;
 		}
-		if(test = getch()){	
+		if((test = getch())){	
 			clear();				//Am Anfang das Fenster leeren, ansonsten werden Eingaben aneinander gereiht
 			refresh();
-			attron(COLOR_PAIR(1));
-			printw("The Key pressed is: the %c character", test);	//Eingabe auf Bildschirm wiedergeben
+			
+			attron(COLOR_PAIR(selectColour(test)));
+			printw("Current Pair NR: %d The Key pressed is: the %c character",selectColour(test), test);	//Eingabe auf Bildschirm wiedergeben
 			refresh();
-			attroff(COLOR_PAIR(1));				
+			attroff(COLOR_PAIR(selectColour(test)));				
 		}
 	}
 }
+int selectColour(char c){
+	int selectedPair = 1;
+	switch (c) {
+		case 'a':
+		case 's':
+		case 'd':
+		return selectedPair = 2;
 
+		case 'y':
+		case 'x':
+		case 'c':
+		return selectedPair = 3;
+
+		case 'w':
+		case 'e':
+		case 'r':
+		case 't':
+		return selectedPair = 4;
+		
+		case 'f':
+		case 'g':
+		case 'h':
+		case 'j':
+		return selectedPair = 5;
+
+		default:
+		return selectedPair;
+	}	
+}
